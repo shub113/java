889650a20f7ac24d323c73d45ac226a5832ff202
Shubham Thakur <shub_mac@Shubhams-MacBook-Pro.local>
2025-07-17 18:39:05 +0530

    minor change

--- MoveZero.java
+++ MoveZero.java
@@ -1,34 +1,25 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-
-class MoveZero
-{
+class MoveZero {
 
     // Space -> O(1)
     // Time -> O(n)
     public static int[] shift(int[] arr) {
-       int nonZeroIndex = 0;
-       int index =0;
-       while(index<arr.length){
-           if(arr[index]!= 0){
-               arr[nonZeroIndex++] = arr[index];
-               arr[index]= 0;
-           }
-           index++;
-       }
-       return arr;
+        int nonZeroIndex = 0;
+        int index = 0;
+        while (index < arr.length) {
+            if (arr[index] != 0) {
+                arr[nonZeroIndex++] = arr[index];
+                arr[index] = 0;
+            }
+            index++;
+        }
+        return arr;
     }
 
-   
-
-    public static void main(String[] args) throws java.lang.Exception
-    {
-       int[] ans = shift(new int[]{0,0,2,0,3,4,0,45});
-       for(int i:ans){
-           System.out.println(i+", ");
-       }
+    public static void main(String[] args) throws java.lang.Exception {
+        int[] ans = shift(new int[] { 0, 0, 2, 0, 3, 4, 0, 45 });
+        for (int i : ans) {
+            System.out.println(i + ", ");
+        }
 
     }
 }

aa6ef56f897ab4a73e7cde1beac2c0ef26a4715a
Shubham Thakur <shub_mac@Shubhams-MacBook-Pro.local>
2025-07-18 16:48:55 +0530

    stack insert at bottom

---
+++ StackInsertAtBottom.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class StackInsertAtBottom
+{
+
+    public static Stack < Integer > insert(Stack < Integer > stack, int num) {
+        if (stack.isEmpty()) {
+            stack.push(num);
+        } else {
+            int popedNum = stack.pop();
+            insert(stack, num);
+            stack.push(popedNum);
+        }
+        return stack;
+    }
+
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        Stack < Integer > stack = new Stack < > ();
+        stack.push(5);
+        stack.push(7);
+        stack.push(1);
+        stack.push(3);
+
+        insert(stack, 9);
+
+        System.out.println(stack);
+
+    }
+}

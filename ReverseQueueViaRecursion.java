02561fb0a15838ed9c99a6a92efb17e4b02fddde
Shubham Thakur <shub_mac@Shubhams-MacBook-Pro.local>
2025-07-20 14:56:42 +0530

    reverse queue using recursion

---
+++ ReverseQueueViaRecursion.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class ReverseQueueViaRecursion
+{
+
+    public static void reverse(Queue < Integer > queue) {
+        if (queue.isEmpty()) {
+            return;
+        }
+
+        // remove from rear 
+        int element = queue.poll();
+
+        // reverse remaining queue
+        reverse(queue);
+
+        // add to front
+        queue.offer(element);
+    }
+
+
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        Queue < Integer > queue = new LinkedList < > ();
+
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        queue.offer(5);
+
+        System.out.println("Original - " + queue);
+
+        reverse(queue);
+
+        System.out.println("Reversed - " + queue);
+
+
+    }
+}
